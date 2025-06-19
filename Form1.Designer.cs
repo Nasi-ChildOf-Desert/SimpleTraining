3e2183540a61b04e04464b89dc050e4f5ab53096
Nyusha <nasi.childof.deseart@gmail.com>
2025-06-19 18:39:57 +0330

    First Edition of code

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace CaesarCiper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtsecured = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(582, 295);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 22);
+            this.txtFile.Multiline = true;
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(645, 97);
+            this.txtFile.TabIndex = 1;
+            // 
+            // txtsecured
+            // 
+            this.txtsecured.Location = new System.Drawing.Point(12, 125);
+            this.txtsecured.Multiline = true;
+            this.txtsecured.Name = "txtsecured";
+            this.txtsecured.Size = new System.Drawing.Size(645, 97);
+            this.txtsecured.TabIndex = 1;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 238);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(645, 42);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 354);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtsecured);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtsecured;
+        private System.Windows.Forms.TextBox txtMsg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+namespace CaesarCiper
+{
+    public partial class Form1 : Form
+    {
+        String line;
+        StringBuilder stb, stbS;
+        string[] lineWords;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ReadFileToStringBuilder();
+            doCaesarCiper();
+        }
+        private void ReadFileToStringBuilder()
+        {
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("D:\\project\\testCaesar\\ceasar.txt");
+                //Read the first line of text
+                stb = new StringBuilder(sr.ReadToEnd());
+                sr.Close();
+
+                txtFile.Text = stb.ToString(); 
+            }
+            catch (Exception e)
+            {
+                txtMsg.Text = "Exception: " + e.Message;
+            }
+            finally
+            {
+                txtMsg.Text += "finally: \n";
+            }
+
+        }
+        private void doCaesarCiper()
+        {
+            stbS = new StringBuilder();
+            int nShift = 3;
+            int i = 0;
+            Char ch;
+            while (i < stb.Length)
+            {
+                ch = stb[i];
+                if (Char.IsLetter(stb[i]))
+                {
+                    if (Char.IsUpper(stb[i]))
+                    {
+                        ch = (Char)(((int)stb[i] + nShift - 65) % 26 + 65);
+                    }
+                    else if (Char.IsLetter(stb[i]) && Char.IsLower(stb[i]))
+                    {
+                        ch = (Char)(((int)stb[i] + nShift - 97) % 26 + 97);
+                    }
+                   
+                }
+                i++;
+
+                stbS.Append(ch);
+            }
+            txtsecured.Text = stbS.ToString();
+            txtMsg.Text += "doCaesarCiper:\n ";
+            Thread.Sleep(2000);
+        }
+        private void ReadFile()
+        {
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("D:\\project\\testCaesar\\ceasar.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    txtFile.Text += line;
+                    //Read the next line
+                    line = sr.ReadLine();
+                    // lineWords = line.Split(' ');
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+                txtMsg.Text += "Read Finished \n";
+            }
+            catch (Exception e)
+            {
+                txtMsg.Text = "Exception: " + e.Message;
+            }
+            finally
+            {
+                txtMsg.Text += "finally: \n";
+            }
+            Thread.Sleep(2000);
+        }
+
+    }
+}
