c2e78de3c27610ccbefe6abf23d3b7c92933a175
Nyusha <nasi.childof.deseart@gmail.com>
2025-07-14 19:29:50 +0330

    Fix some bug in reading file and Write to file

--- Form1.cs
+++ Form1.cs
@@ -13,15 +13,18 @@ namespace CaesarCiper
 {
     public partial class Form1 : Form
     {
-        String line;
-        StringBuilder stb, stbS, stbUnS;
+        String line, writeStr;
+        StringBuilder stb , stbUnS;
+        StringBuilder stbS;
         string[] lineWords;
         Thread thReadFile;
         Thread thDecode;
+        Thread thWriteFile;
         private bool _run;
         private bool isReadCompleted = false;
         private readonly object lockStb = new Object();
         public event EventHandler<StringBuilder> DataReceived;
+        public event EventHandler<String> ReadyTowrite;
 
         public Form1()
         {
@@ -32,56 +35,150 @@ namespace CaesarCiper
         private void initThreads()
         {
             thReadFile = new Thread(ReadFileToStringBuilder);
+            thReadFile.IsBackground = true;
             thDecode = new Thread(doCaesarCiper);
             _run = false;
 
+            thWriteFile = new Thread(WriteStringToFile);
+            thWriteFile.IsBackground = true;
+
             DataReceived += startProcess;
+            ReadyTowrite += startTowrite;
+
+        }
+
+        private void WriteStringToFile()
+        {
+
+            var _sampleFilePath = "D:\\project\\testCaesar\\ceasarcpded.txt";
+            File.WriteAllText(_sampleFilePath, writeStr);
+            txtMsg.Invoke(new Action(() =>
+            {
+                txtMsg.Text = "wited: " ;
+            }));
+        }
+
+        private void startTowrite(object sender, string str)
+        {
+            writeStr = str;
+            thWriteFile.Start();
 
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
             thReadFile.Start();
-          
-           
-
         }
         private void ReadFileToStringBuilder()
         {
+            #region .:Read all file in one command:.
+            /* try
+             {
+                 StreamReader sr = new StreamReader("D:\\project\\testCaesar\\ceasar.txt");
+                 //what is best scope for lock ?? just line 55 or all of them till line 63?
+
+                     stb = new StringBuilder(sr.ReadToEnd());
+
+                     sr.Close();
+
+                     txtFile.Invoke(new Action(() =>
+                     {
+                         txtFile.Text = stb.ToString();
+                     }));
+
+
+                 DataReceived(this, stb);
+             }
+             catch (Exception e)
+             {
+
+                 txtMsg.Invoke(new Action(() =>
+                  {
+                      txtMsg.Text = "Exception: " + e.Message;
+                  }));
+             }
+             finally
+             {
+                 txtMsg.Invoke(new Action(() =>
+                 {
+                     txtMsg.Text += "finally: \n";
+                 }));
+             } */
+            #endregion
+
             try
             {
-                StreamReader sr = new StreamReader("D:\\project\\testCaesar\\ceasar.txt");
-                //what is best scope for lock ?? just line 55 or all of them till line 63?
-                lock (lockStb)
+                var _sampleFilePath = "D:\\project\\testCaesar\\ceasar.txt";
+                var streamReader = new StreamReader(_sampleFilePath);
+
+                // Part 1: create new FileInfo get Length.
+                FileInfo info = new FileInfo(_sampleFilePath);
+                long length = info.Length;
+                long fileSizeinMbs = length / (1024 * 1024);
+
+                var stb1 = new StringBuilder();
+                string fileLine;
+                while ((fileLine = streamReader.ReadLine()) != null)
                 {
-                    stb = new StringBuilder(sr.ReadToEnd());
+                    stb1.AppendLine(fileLine);
+                }
 
-                    sr.Close();
+                var sst = stb1.ToString();
 
+                if (fileSizeinMbs > 1)
+                {
+
+                    #region .:Invoke all string to UI that it was wrong :.
+
+                    /* int part_size = (int)(length / fileSizeinMbs); 
+                     * var offSet = sst.Length / (int)part_size;
+                    */
+                    /* it was wrong coz ui hanged
+                      for (int i = 0; i < part_size; i++)
+                    {
+                        var part = sst.Substring(i * offSet, offSet);
+                        txtFile.Invoke(new Action(() =>
+                        {
+                            txtFile.Text += part;
+                        }));
+                    }
+                     */
+                    #endregion
+                    // just show the last 1 Mb
+                    var offSet = sst.Length / (int)fileSizeinMbs;
+                    var part = sst.Substring((sst.Length - offSet), offSet);
                     txtFile.Invoke(new Action(() =>
                     {
-                        txtFile.Text = stb.ToString();
+                        txtFile.Text = part;
                     }));
 
                 }
-                DataReceived(this, stb);
+                else
+                {
+                    txtFile.Invoke(new Action(() =>
+                    {
+                        txtFile.Text = sst;
+                    }));
+
+                }
+
+                DataReceived(this, stb1);
             }
             catch (Exception e)
             {
 
                 txtMsg.Invoke(new Action(() =>
-                 {
-                     txtMsg.Text = "Exception: " + e.Message;
-                 }));
+                {
+                    txtMsg.Text = "Exception: " + e.Message;
+                }));
             }
             finally
             {
                 txtMsg.Invoke(new Action(() =>
                 {
-                    txtMsg.Text += "finally: \n";
+                    txtMsg.Text += "All data of File has read: \n";
                 }));
             }
-
         }
         private void startProcess(object sender, StringBuilder stbGet)
         {
@@ -97,11 +194,15 @@ namespace CaesarCiper
             {
                 if (!isReadCompleted)
                 {
-                   Thread.Sleep(20);
+                    Thread.Sleep(20);
                     continue;
                 }
-                lock (lockStb)
+
                 {
+                    txtMsg.Invoke(new Action(() =>
+                    {
+                        txtMsg.Text += "Start encoding ... : \n";
+                    }));
                     stbS = new StringBuilder();
                     int nShift = 3;
                     int i = 0;
@@ -136,15 +237,21 @@ namespace CaesarCiper
                         }));
 
                     }
+                  
+                    var stSecured = stbS.ToString();
+                    var part = stSecured.Substring((stSecured.Length - 500), 500);
                     txtsecured.Invoke(new Action(() =>
                     {
-                        txtsecured.Text = stbS.ToString();
+
+                        txtsecured.Text = part;
                         txtMsg.Text += "doCaesarCiper:\n ";
                     }));
-                    
+
                     stb.Clear();
-                     stb = null;
-                     _run = false;
+                    stb = null;
+                    _run = false;
+                    
+                    ReadyTowrite(this, stbS.ToString());
                     thDecode.Abort();
                 }
                 Thread.Sleep(2000);
